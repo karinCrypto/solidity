9cb4eb6fc9529858d871892fcbfaa87f54418b51
starlash7 <yang92sw@gmail.com>
2024-07-23 22:40:52 +0900

    Q7 return 위치 수정

--- solidity100/test1.sol
+++ solidity100/test1.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.8.2 < 0.9.0;
 
-contract TEST1 {
+contract Q1 {
     /*
     1. 더하기, 빼기, 곱하기, 나누기 그리고 제곱을 반환받는 계산기를 만드세요.
     */
@@ -23,7 +23,7 @@ contract TEST1 {
     }
 }
 
-contract TEST2 {
+contract Q2 {
     /*
     2개의 Input값을 가지고 1개의 output값을 가지는 4개의 함수를 만드시오. 각각의 함수는 더하기, 빼기, 곱하기, 나누기(몫)를 실행합니다. */
      function Plus(uint _a, uint _b) public pure returns(uint){
@@ -40,7 +40,7 @@ contract TEST2 {
      }
 }
 
-    contract TEST3{
+    contract Q3{
         /*1개의 Input값을 가지고 1개의 output값을 가지는 2개의 함수를 만드시오. 각각의 함수는 제곱, 세제곱을 반환합니다.*/
         function Mul2(uint _n) public pure returns(uint) {
             return _n**2;
@@ -50,7 +50,7 @@ contract TEST2 {
         }
     }
 
-    contract TEST4{
+    contract Q4{
         /*4. 이름(string), 번호(uint), 듣는 수업 목록(string[])을 담은 student라는 구조체를 만들고 그 구조체들을 관리할 수 있는 array, students를 선언하세요.*/
         struct Student {
             string name;
@@ -60,7 +60,7 @@ contract TEST2 {
         Student[] Students;
     }
 
-   contract TEST5{
+   contract Q5{
         /*
         5. 아래의 함수를 만드세요
         1~3을 입력하면 입력한 수의 제곱을 반환받습니다.
@@ -78,9 +78,9 @@ contract TEST2 {
         return 0;  
     }
 }
+   }
 
-contract TEST6 {
-
+contract Q6 {
     /*6. 숫자만 들어갈 수 있는 array numbers를 만들고 그 array안에 0부터 9까지 자동으로 채우는 함수를 구현하세요.(for 문)*/
     uint[] numbers;
     
@@ -94,7 +94,7 @@ contract TEST6 {
     }
 }
 
-contract TEST7 {
+contract Q7 {
     /*
     7. 숫자만 들어갈 수 있는 array numbers를 만들고 그 array안에 0부터 5까지 자동으로 채우는 함수와 array안의 모든 숫자를 더한 값을 반환하는 함수를 각각 구현하세요.(for 문)*/
        uint[] numbers;
@@ -118,7 +118,7 @@ contract TEST7 {
     }
 }
 
-    contract TEST8 {
+    contract Q8 {
         /*
         아래의 함수를 만드세요
         1~10을 입력하면 “A” 반환받습니다.
@@ -136,15 +136,16 @@ contract TEST7 {
                 return "nope";
             }
         }
-    
-    contract TEST9 {
+    }
+
+    contract Q9 {
         /*9.문자형을 입력하면 bytes 형으로 변환하여 반환하는 함수를 구현하세요.*/
         function change(string memory _n)public pure returns(bytes memory) {
             return bytes(_n);
         }
     }
 
-    contract TEST10 {
+    contract Q10 {
         /*10. 숫자만 들어가는 array numbers를 선언하고 숫자를 넣고(push), 빼고(pop), 특정 n번째 요소의 값을 볼 수 있는(get)함수를 구현하세요.*/
         uint[] numbers;
 
