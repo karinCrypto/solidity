3f883fb9d1af18a7286e5d1305127d0796cfdde4
starlash7 <yang92sw@gmail.com>
2024-07-24 01:51:28 +0900

    test2 답수정

--- solidity100/test2.sol
+++ solidity100/test2.sol
@@ -2,20 +2,20 @@
 
 pragma solidity >=0.8.2 < 0.9.0;
 
-contract TEST1 {
+contract Q11 {
     /*
     1. uint 형이 들어가는 array를 선언하고, 짝수만 들어갈 수 있게 걸러주는 함수를 구현하세요.
     */
     uint[] numbers;
 
-    function test(uint _n) public {
+    function even(uint _n) public {
         if (_n % 2 == 0) {
             numbers.push(_n);
         }
     }
 }
 
-contract TEST2 {
+contract Q12 {
     /*
     2. 숫자 3개를 더해주는 함수, 곱해주는 함수 그리고 순서에 따라서 a*b+c를 반환해주는 함수 3개를 각각 구현하세요.
     */
@@ -31,24 +31,27 @@ contract TEST2 {
     }
 }
 
-contract TEST3 {
+contract Q13 {
     /*
     3. 3의 배수라면 “A”를, 나머지가 1이 있다면 “B”를, 나머지가 2가 있다면 “C”를 반환하는 함수를 구현하세요.
     */
-    function three(uint _n) public pure returns(string memory) {
-        if(_n%3 == 0) {
-            return "A";
-        } else if(_n%3 == 1) {
-            return "B";
-        } else {
-            return "C";
+    function getNumber(uint _n) public pure returns(string memory) {
+            if(_n % 3 == 0){
+                return "A";
+            }else if(_n % 3 == 1){
+                return "B";
+            }else if(_n % 3 == 2){
+                return "C";
+            }else{
+                return "0";
         }
     }
 }
 
-contract TEST4 {
+contract Q14 {
     /*
-    4. 학번, 이름, 듣는 수험 목록을 포함한 학생 구조체를 선언하고 학생들을 관리할 수 있는 array를 구현하세요. array에 학생을 넣는 함수도 구현하는데 학생들의 학번은 1번부터 순서대로 2,3,4,5 자동 순차적으로 증가하는 기능도 같이 구현하세요.
+    4. 학번, 이름, 듣는 수험 목록을 포함한 학생 구조체를 선언하고 학생들을 관리할 수 있는 array를 구현하세요. 
+    array에 학생을 넣는 함수도 구현하는데 학생들의 학번은 1번부터 순서대로 2,3,4,5 자동 순차적으로 증가하는 기능도 같이 구현하세요.
     */
       struct Student {
         uint number;
@@ -66,7 +69,7 @@ contract TEST4 {
     }
 }
 
-contract TEST5 {
+contract Q15 {
     /*
     5. 배열 A를 선언하고 해당 배열에 0부터 n까지 자동으로 넣는 함수를 구현하세요. 
     */
@@ -79,7 +82,7 @@ contract TEST5 {
     }
 }
 
-contract TEST6 {
+contract Q16 {
     /*
     6. 숫자들만 들어갈 수 있는 array를 선언하고 해당 array에 숫자를 넣는 함수도 구현하세요. 또 array안의 모든 숫자의 합을 더하는 함수를 구현하세요.
     */
@@ -98,7 +101,7 @@ contract TEST6 {
     }
 }
 
-contract TEST7 {
+contract Q17 {
     /*
     7. string을 input으로 받는 함수를 구현하세요. 이 함수는 true 혹은 false를 반환하는데 Bob이라는 이름이 들어왔을 때에만 true를 반환합니다. 
     */
@@ -107,41 +110,41 @@ contract TEST7 {
     } 
 }
 
-contract TETS8 {
+contract Q18 {
     /*
     8. 이름을 검색하면 생일이 나올 수 있는 자료구조를 구현하세요. (매핑) 해당 자료구조에 정보를 넣는 함수, 정보를 볼 수 있는 함수도 구현하세요.
     */
-    mapping(string => uint) test;
+    mapping(string => uint) birthday;
 
-    function test8(string memory _name, uint _test)public {
-        test[_name] = _test;
+    function getBirthday(string memory _name, uint _birthday)public {
+        birthday[_name] = _birthday;
     }
 }
 
-contract TEST9 {
+contract Q19 {
     /*
     9. 숫자를 넣으면 2배를 반환해주는 함수를 구현하세요. 단 숫자는 1000이상 넘으면 함수를 실행시키지 못하게 합니다.
     */
-   function twice(uint _n)public pure returns(uint) {
+   function getTwice(uint _n)public pure returns(uint) {
     require(_n<1000);
     return _n * 2;
    } 
 }
 
-contract TEST10 {
+contract Q20 {
     /*
-    10. 숫자만 들어가는 배열을 선언하고 숫자를 넣는 함수를 구현하세요. 15개의 숫자가 들어가면 3의 배수 위치에 있는 숫자들을 초기화 시키는(3번째, 6번째, 9번째 등등) 함수를 구현하세요. (for 문 응용 → 약간 까다로움)
+    10. 숫자만 들어가는 배열을 선언하고 숫자를 넣는 함수를 구현하세요. 
+    15개의 숫자가 들어가면 3의 배수 위치에 있는 숫자들을 초기화 시키는(3번째, 6번째, 9번째 등등) 함수를 구현하세요. 
+    (for 문 응용 → 약간 까다로움)
     */
     uint[] numbers;
-    function ten(uint _n) public {
-        require(numbers.length<15);
+    function pushNumber(uint _n) public {
         numbers.push(_n);
+
+        if(numbers.length==15){
+            for(uint i=2; i<numbers.length; i+=3){
+                numbers[i] = 0;
     }
-    function tenten() public {
-        for(uint i=0; i<numbers.length; i++) {
-            if(_n % 3 ==0) {
-                
-            }
-        }
+}
     }
 }
