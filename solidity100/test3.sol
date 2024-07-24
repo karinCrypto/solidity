e76463b660dd2a04c8fb01c6946816b438ae5157
starlash7 <yang92sw@gmail.com>
2024-07-24 10:19:41 +0900

    test3,4

--- solidity100/test3.sol
+++ solidity100/test3.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.8.2 < 0.9.0;
 
-contract TEST1 {
+contract Q21 {
     /*
     1. 3의 배수만 들어갈 수 있는 array를 구현하세요.
     */
@@ -14,7 +14,7 @@ contract TEST1 {
     }
 }
 
-contract TEST2 {
+contract Q22 {
     /*
     2. 뺄셈 함수를 구현하세요. 임의의 두 숫자를 넣으면 자동으로 둘 중 큰수로부터 작은 수를 빼는 함수를 구현하세요.
     
@@ -25,8 +25,9 @@ contract TEST2 {
             
         }
      }
+}
 
-contract TEST3 {
+contract Q23 {
     /*
     3. 3의 배수라면 “A”를, 나머지가 1이 있다면 “B”를, 나머지가 2가 있다면 “C”를 반환하는 함수를 구현하세요.
     */
@@ -41,7 +42,7 @@ contract TEST3 {
     }
 }
 
-contract TEST4 {
+contract Q24 {
     /*
     4. string을 input으로 받는 함수를 구현하세요. “Alice”가 들어왔을 때에만 true를 반환하세요.
     */
@@ -50,7 +51,7 @@ contract TEST4 {
     }
 }
 
-contract TEST5 {
+contract Q25 {
     /*
     5. 배열 A를 선언하고 해당 배열에 n부터 0까지 자동으로 넣는 함수를 구현하세요. 
     */
@@ -62,7 +63,7 @@ contract TEST5 {
 
 }
 
-contract TEST6 {
+contract Q26 {
     /*
     6. 홀수만 들어가는 array, 짝수만 들어가는 array를 구현하고 숫자를 넣었을 때 자동으로 홀,짝을 나누어 입력시키는 함수를 구현하세요.
     */
@@ -79,7 +80,7 @@ contract TEST6 {
 
 }
 
-contract TEST7 {
+contract Q27 {
     /*
     7. string 과 bytes32를 key-value 쌍으로 묶어주는 mapping을 구현하세요. 해당 mapping에 정보를 넣고, 지우고 불러오는 함수도 같이 구현하세요.
     */
@@ -100,7 +101,7 @@ contract TEST7 {
 
 }
 
-contract TEST8 {
+contract Q28 {
     /*
     8. ID와 PW를 넣으면 해시함수(keccak256)에 둘을 같이 해시값을 반환해주는 함수를 구현하세요.
     */
@@ -109,7 +110,7 @@ contract TEST8 {
     }
 }
 
-contract TEST9 {
+contract Q29 {
     /*
     9. 숫자형 변수 a와 문자형 변수 b를 각각 10 그리고 “A”의 값으로 배포 직후 설정하는 contract를 구현하세요.
     */
@@ -122,7 +123,7 @@ contract TEST9 {
     }
 }
 
-contract TEST10 {
+contract Q30 {
     /*
     10. 임의대로 숫자를 넣으면 알아서 내림차순으로 정렬해주는 함수를 구현하세요
 (sorting 코드 응용 → 약간 까다로움)
